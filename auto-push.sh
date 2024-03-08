03cc63b23621ecff26723143463e9dd73113efc9
luqin2007 <lq2007lq@hotmail.com>
2024-03-09 04:14:33 +0800

    auto-push

--- auto-push.sh
+++ auto-push.sh
@@ -2,3 +2,4 @@ git add -A
 git commit -m "auto-push"
 git push -f
 
+/home/lq2007/ossutil/ossutil64 sync oss://notebook-backup-lq2007/siyuan /siyuan/workspace/data/assets --update
--- run_local_app.sh
+++ run_local_app.sh
@@ -1,7 +1,7 @@
 sudo docker stop 56380b31e4c1
 echo push current notes, please waiting...
 git add -A
-git commit -m "auto-push"
+git commit -m "auto-push(before local)"
 git push -f
 echo waiting update, press any key to continue...
 read temp
