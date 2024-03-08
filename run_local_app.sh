be84958ab76d6125d81cbcddcd2abeb1c0ab31b4
luqin2007 <lq2007lq@hotmail.com>
2024-03-09 03:30:40 +0800

    auto-push

---
+++ run_local_app.sh
@@ -0,0 +1,10 @@
+sudo docker stop 56380b31e4c1
+echo push current notes, please waiting...
+git add -A
+git commit -m "auto-push"
+git push -f
+echo waiting update, press any key to continue...
+read temp
+git pull
+sudo docker start 56380b31e4c1
+
