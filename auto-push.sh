fe2f7c15aa83f2e520db6fb5f8208e2e6bdf2e7e
luqin2007 <lq2007lq@hotmail.com>
2024-03-10 11:29:20 +0800

    auto-push

--- auto-push.sh
+++ auto-push.sh
@@ -1,5 +1,6 @@
-git add -A
-git commit -m "auto-push"
-git push -f
+echo begin > /siyuan/log
+git add -A >> /siyuan/log
+git commit -m "auto-push" >> /siyuan/log
+git push -f >> /siyuan/log
 
-/home/lq2007/ossutil/ossutil64 sync /siyuan/workspace/data/assets oss://notebook-backup-lq2007/siyuan --update -f
+/home/lq2007/ossutil/ossutil64 sync /siyuan/workspace/data/assets oss://notebook-backup-lq2007/siyuan --update -f >> /siyuan/log
